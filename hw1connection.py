9d60e97e6cf55508a1c67c6035ef2879a00e07fc
mipreza <miichelleperez@yahoo.com>
2021-02-27 04:35:12 -0600

    sql code implemented

--- hw1connection.py
+++ hw1connection.py
@@ -35,59 +35,82 @@ def execute_read_query(connection, query):
         print(f"The error '{e}' occurred")
 
 connection = create_connection("cis3368dbid.cc9zm0jrngaw.us-east-1.rds.amazonaws.com", "mperez39", "Krowczyk0531!", "CIS3368db")
-
-
+        
+    # this is the simple menu function
+ 
 class Contacts:
-    contactList = []
+    contact_list = []
+    def main():
+        print("""
+        MENU
+        a - Add contact
+        d - Remove contact
+        u - Update contact details
+        b - Output all contacts in alphabetical order
+        c - Output all contacts by creation date
+        o - Output all contacts
+        q - Quit
+
+        Choose an option: """)
+        ans = input()
+
     def __init__(self, id, contact_details, creation_date):
         self.id = id
-        self.contactDetails = contactDetails
-        self.creationDate = creationDate
+        self.contact_details = contact_details
+        self.creation_date = creation_date
 
-    def menu(): 
-        ans = True
-        while ans:
-    # this is the simple menu function
-            print("--------------------------------------------------------------------") 
-            print("\t\t\tMENU", flush=False) 
-            print("--------------------------------------------------------------------")  
-            print("a - Add contact") 
-            print("d - Remove contact") 
-            print("u - Update contact details") 
-            print("b - Output all contacts in alphabetical order") 
-            print("c - Output all contacts by creation date") 
-            print("o - Output all contacts") 
-            print("q - Quit") 
-
-        ans = int(input("Choose an option: ")) 
-      
-        return ans
 
     def add_contact(self):
         if ans=='a':
             print("\nContact added")
+        query = "INSERT INTO contacts (, contactDetails, creationDate) VALUES (, '%s', %s)"
+        execute_query(connection, query)
+        
+
     def remove_contact(self):
-        elif ans=='d':
+        if ans=='d':
             print("\nContact removed")
+        delete = "DELETE FROM contacts WHERE id = %s"
+        execute_query(connection, delete)
+
     def update_contact(self):
-        elif ans=='u':
+        if ans=='u':
             print("\nContact details updated")
+        update="""
+        UPDATE contact
+        SET contactDetails = %s
+        Where id = 5;
+        """
+        execute_read_query(connection, update)
+
     def output_alphabetic(self):
-        elif ans=='b':
+        if ans=='b':
             print("\nContacts in alphabetical order")
+        alpha ="""
+        SELECT * FROM contacts
+        ORDER_BY contactDetails;
+        """
+        execute_read_query(connection, alpha)
+
     def output_creation_date(self):
-        elif ans=='c':
+        if ans=='c':
             print("\nContacts by creation date")
+        creation ="""
+        SELECT * FROM contacts
+        ORDER_BY creationDate;
+        """
+        execute_read_query(connection, creation)
+
     def output_contacts(self):
-        elif ans=='o':
+        if ans=='o':
             print("\nAll contacts")
         select_contacts = "SELECT * FROM contacts"
         results = execute_read_query(connection, select_contacts)
         for result in results:
             print(result)
 
-    def quit():
-        elif ans=='q':
+    def quit(self):
+        if ans=='q':
             print("\nQuitting...")
             ans==None
 
