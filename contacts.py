2da9fd3e801684709a7a1dc1e9d0aeb9ddbdcadf
mipreza <miichelleperez@yahoo.com>
2021-02-27 20:09:34 -0600

    successful commit with notes

--- contacts.py
+++ contacts.py
@@ -1,43 +1,6 @@
-import mysql.connector
-from mysql.connector import Error
-def create_connection(host_name, user_name, user_password, db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
-        print("Connection to MySQL DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-    return connection
-
-def execute_query(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-        print("Query executed successfully")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-connection = create_connection("cis3368dbid.cc9zm0jrngaw.us-east-1.rds.amazonaws.com", "mperez39", "Krowczyk0531!", "CIS3368db")
-
-select_contacts = "SELECT * FROM contacts"
-results = execute_read_query(connection, select_contacts)
-
-for result in results:
-    print(result)
+class Contacts:
+    contact_list=[]
+    def __init__(self, id, contact_details, creation_date):
+        self.id = id
+        self.contact_details = contact_details
+        self.creation_date = creation_date
